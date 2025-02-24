47dc3dcc3d53cf9607574ec3b18d4a72d6b7f5e5
Yusufbek-uz <yusufbekdeveloper@gmail.com>
2025-02-24 21:24:49 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { Hiddn } from './assets/components/hiden/hiden'
+import { Hiddn } from './assets/components/hiden/Hiden'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
